754fb8cd76bda8e38aafd6a37d59404e0ca6dde5
Louis <lreillywillhight@gmail.com>
2020-06-11 16:55:18 -0700

    updated to phaser.physics.p2

--- mazeRun/script.js
+++ mazeRun/script.js
@@ -19,7 +19,9 @@ var cursors
 
 function create() {
 
-  game.physics.startSystem(Phaser.Physics.ARCADE)
+  game.physics.startSystem(Phaser.Physics.P2JS)
+
+  game.physics.p2.restitution = .8
 
   game.stage.backgroundColor = '#787878'
   
@@ -31,49 +33,58 @@ function create() {
 
   layer = map.createLayer('tileLayer1')
 
-  layer.debug = true
   
   layer.resizeWorld()
+
+  game.physics.p2.convertTilemap(map, layer)
   
   player = game.add.sprite((8 * tileWidth), (7 * tileHeight),'player')
   
-  game.physics.enable(player, Phaser.Physics.ARCADE)
+  game.physics.p2.enable(player)
+  
+  // player.body.damping()
+  player.body.fixedRotation = true
+  player.body.setCircle(24,0,3)
   
   // game.physics.arcade.gravity.x = 199 
   
   // player.body.bounce.y = .5
   // player.body.bounce.x = .5
   // player.body.linearDamping = .5
-  player.body.collideWorldBounds = true
-  player.body.setSize(24, 24, 24, 32)
-  player.body.debug = true
+  // player.body.collideWorldBounds = true
+  // player.body.setSize(24, 24, 24, 32)
+  game.physics.p2.setBoundsToWorld(true, true, true, true, false)
+  
   
   game.camera.follow(player)
-
+  
   cursors = game.input.keyboard.createCursorKeys()
-
+  
 }
 
 function update() {
-
-  game.physics.arcade.collide(player, layer)
-  player.body.velocity.x = 0
-  player.body.velocity.y = 0
-
+  
+  // game.physics.arcade.collide(player, layer)
+  // player.body.velocity.x = 0
+  // player.body.velocity.y = 0
+  player.body.setZeroVelocity()
+  
   if (cursors.up.isDown) {
-    player.body.velocity.y = -200
+    player.body.moveUp(300)
   }
   else if (cursors.left.isDown) {
-    player.body.velocity.x = -200
+    player.body.moveLeft(300)
   }
   else if (cursors.down.isDown) {
-    player.body.velocity.y = 200
+    player.body.moveDown(300)
   }
   else if (cursors.right.isDown) {
-    player.body.velocity.x = 200
+    player.body.moveRight(300)
   }
 }
 
 function render() {
   // game.debug.bodyinfo()
+  player.body.debug = true
+  layer.debug = true
 }
