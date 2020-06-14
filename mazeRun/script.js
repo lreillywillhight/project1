85b33740e0a57f0aeb74abf0df874238942270ee
Louis <lreillywillhight@gmail.com>
2020-06-13 19:09:02 -0700

    added a function and button to clear playable space. Next is map generation.

--- mazeRun/script.js
+++ mazeRun/script.js
@@ -1,8 +1,8 @@
-  var tileWidth = 64
-  var tileHeight = 64
+var tileWidth = 64
+var tileHeight = 64
 
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update, render: render})
-  // var game = new Phaser.Game((24 * tileWidth), (16 * tileHeight), Phaser.AUTO, '', {preload: preload, create: create, update: update, render: render})
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render })
+// var game = new Phaser.Game((24 * tileWidth), (16 * tileHeight), Phaser.AUTO, '', {preload: preload, create: create, update: update, render: render})
 
 
 function preload() {
@@ -24,51 +24,70 @@ function create() {
   game.physics.p2.restitution = .8
 
   game.stage.backgroundColor = '#787878'
-  
+
   map = game.add.tilemap('newMap')
 
   map.addTilesetImage('tileset', 'tiles')
 
-  map.setCollisionBetween(2,3)
+  map.setCollisionBetween(2, 3)
 
   layer = map.createLayer('tileLayer1')
 
-  
+
   layer.resizeWorld()
 
   game.physics.p2.convertTilemap(map, layer)
-  
-  player = game.add.sprite((8 * tileWidth), (7 * tileHeight),'player')
-  
+
+  player = game.add.sprite((8 * tileWidth), (7 * tileHeight), 'player')
+
   game.physics.p2.enable(player, false)
-  
+
   // player.body.damping()
   player.body.fixedRotation = true
-  player.body.setCircle(20,0,7)
-  
+  player.body.setCircle(20, 0, 7)
+
   // game.physics.arcade.gravity.x = 199 
-  
+
   // player.body.bounce.y = .5
   // player.body.bounce.x = .5
   // player.body.linearDamping = .5
   // player.body.collideWorldBounds = true
   // player.body.setSize(24, 24, 24, 32)
   game.physics.p2.setBoundsToWorld(true, true, true, true, false)
-  
-  
+
+
   game.camera.follow(player)
-  
+
   cursors = game.input.keyboard.createCursorKeys()
-  
+
 }
 
+//CLEARMAP
+//removes all tiles (except outer walls) from map
+//map.removeTile(x,y) //x,y is tile
+//map.width; map.height //x,y is tile
+//map.getTile(x,y) // xy is tile
+
+function clearMap() {
+  for (let x = 1; x < map.width - 1; x++) {
+    for (let y = 1; y < map.height - 1; y++) {
+      map.removeTile(x, y)
+    }
+  }
+  game.physics.p2.convertTilemap(map,layer)
+}
+
+document.getElementById("clearMapButton").addEventListener("click", function(e) {
+  clearMap()
+})
+
 function update() {
-  
+
   // game.physics.arcade.collide(player, layer)
   // player.body.velocity.x = 0
   // player.body.velocity.y = 0
   player.body.setZeroVelocity()
-  
+
   if (cursors.right.isDown) {
     player.body.moveRight(300)
   }
